d5dc265543515c1d3b4dc83135531802b63c4acd
jkevinturado <jkevinturado@gmail.com>
2026-05-06 19:51:49 +0800

    Removed build files and added compiled code to .gitignore

--- dist/helpers/common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomNumberByLength = void 0;
-function getRandomNumberByLength(max) {
-    return Math.floor(Math.random() * max);
-}
-exports.getRandomNumberByLength = getRandomNumberByLength;
-//# sourceMappingURL=common.js.map
